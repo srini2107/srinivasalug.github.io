349a50e0266f3e34053c49764326b7e7779ebb86
srini2107 <srinivasalu2107@gmail.com>
2025-12-11 18:54:11 +0530

    git add .github/workflows/ci.yml git commit -m "Add CI workflow" git push origin main

---
+++ src/main/java/com/aws/lambda/application/ApiLambdaHandler.java
@@ -0,0 +1,20 @@
+package com.aws.lambda.application;
+
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import java.util.Map;
+
+public class ApiLambdaHandler implements RequestHandler<Map<String, Object>, String> {
+
+    @Override
+    public String handleRequest(Map<String, Object> input, Context context) {
+        context.getLogger().log("Input: " + input);
+
+        // Example: extract path parameter "id"
+        Map<String, String> pathParams = (Map<String, String>) input.get("pathParameters");
+        String id = pathParams != null ? pathParams.get("id") : "unknown";
+
+        return "Customer ID received: " + id;
+    }
+}
---
+++ src/main/java/com/aws/lambda/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.aws.lambda.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+    @GetMapping("/msg")
+    public String getMessage() {
+        return "Hello this is your message";
+    }
+}
